51b0f8505a7678dd933104ee635e3fde9133f5a8
Pham Hien <hienphamantruong@gmail.com>
2020-06-16 15:59:41 +0700

    Module_Hien

---
+++ 1749-detail/1749.php
@@ -0,0 +1,38 @@
+<?php
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+
+require_once'scss.inc.php';
+use ScssPhp\ScssPhp\Compiler;
+$scss = new Compiler();
+$str = file_get_contents('sass/1749-styles.scss');
+$str = $scss->compile($str);
+file_put_contents('css/1749-styles.css', $str);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="./css/styles.css"> -->
+    <link href="<?php echo $url_path ?>/css/1749-styles.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $url_path ?>/fontawesome-free-5.13.0-web/css/all.min.css" rel="stylesheet"/>
+    <link href="<?php echo $url_path ?>/swiper-5.4.1/package/css/swiper.min.css" rel="stylesheet"/>
+    <link href="<?php echo $url_path ?>/family-font/Roboto-Black.ttf" rel="stylesheet"/>
+    <script src="<?php echo $url_path ?>/swiper-5.4.1/package/js/swiper.min.js"></script>
+    <script src="<?php echo $url_path ?>/js/jquery-3.5.1.min.js"></script>
+    <script language="JavaScript" type="text/javascript" src="<?php echo $url_path ?>/js/scripts.js"></script>
+    <title>Document</title>
+</head>
+<body>
+   <?php
+      include "./1749-detail.php";
+   ?>
+</body>
+</html>
---
+++ 1804-about/1804-about.php
@@ -0,0 +1,50 @@
+<?php
+// $url_host = $_SERVER['HTTP_HOST'];
+
+// $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
+// $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+// preg_match_all($pattern_uri, __DIR__, $matches);
+
+// $url_path = $url_host . $matches[1][0];
+
+// $url_path = str_replace('\\', '/', $url_path);
+
+?>
+
+<div class="type-1804 pt-5 pb-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="more text-left">
+                    <h3 class="story position-relative mb-5 ">Our Story</h3>
+                    <!-- <hr> -->
+                </div>
+               <h5>  Quisque quis ex mattis, euismod mauris lorem ipsum eget, scelerisque sapien.</h5>
+                <p>
+                Quisque quis ex mattis, euismod mauris eget, scelerisque sapien. Curabitur et mattis ante. Maecenas sit amet commodo tellus. Phasellus fermentum pretium eros, ut faucibus velit auctor eget.
+                  </p>
+                  <p>
+                  Vestibulum semper pharetra. Curabitur cursus sapien sed porta dapibus.
+Lorem ipsum – dolor quis ex mattis, euismod mauris eget, scelerisque sapien. Quisque semper malesuada ipsum! Curabitur et mattis ante. Maecenas sit amet commodo tellus.
+                  </p>
+                  <p>Quisque lorem <strong> 12 YEARS </strong>  quis efficitur felis. Duis pharetra <strong> 86 CLIENTS </strong> for amet ultricies augue, nec rhoncus felis <strong>7 AWARDS </strong> nec.</p>
+                <a class="contact font-weight-bold p-2 mr-4" href="#">
+                    <span>Contact us</span>
+                    <i class="fas fa-envelope ml-2"></i>
+                </a>
+                <a class="read-more font-weight-bold p-2" href="#">
+                    <span>View portfolio</span>
+                    <i class="fas fa-briefcase ml-2"></i>
+                </a>
+            </div>
+            <div class="col-sm-6 ">
+                <img width="100%" src="images/Hinh1.jpg " alt="" >
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
